e149071f2faf87dae78e93e54de4b44bc51b2bdf
AyaElgamal90 <60861004+AyaElgamal90@users.noreply.github.com>
2022-07-07 11:43:43 -0400

    Add files via upload

---
+++ Areas/Prototype/Controllers/DMLController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Web_Prototvpe.Areas.Prototype.Models;
+
+namespace Web_Prototvpe.Areas.Prototype.Controllers
+{
+    public class DMLController
+    {
+        public ActionResult Index()
+        {
+            var model = new DMLModel();
+            model.PageTitle = "Digital Micro Loan";
+
+            return View(model);
+        }
+
+        private ActionResult View(DMLModel model)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Areas/Prototype/Models/DMLModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Web_Prototvpe.Areas.Prototype.Models
+{
+    public class DMLModel
+    {
+        public string PageTitle { get; set; }
+        public string PageContent { get; set; }
+        public int PageCount { get; set; }
+        public bool IsHidden { get; set; }
+    }
+}
